218d3622a5bdbe04e25025bfac32ae357648f8ba
东灯 <43312495+cpp-dongdeng@users.noreply.github.com>
2020-02-07 19:57:44 +0800

    Add files via upload

---
+++ NodeCpp.h
@@ -0,0 +1,28 @@
+class Node{
+	public:	
+		string url="";
+		void eval(string js_line){
+			if(url=="")url="eval.js";
+			ofstream out;
+			out.open(url.c_str());
+			out<<js_line;
+			out.close();
+			string rsur="node "+url;
+			system(rsur.c_str());
+		}
+		
+};
+class Node_js{
+	public:
+		string jsurl;
+		void in(string js_line){
+			 if(jsurl=="")jsurl="eval.js";
+			 ofstream file(jsurl.c_str(),ios::app);
+			 file<<js_line;
+			 file.close();
+		}
+		void run(){
+			string rsur="node "+jsurl;
+			system(rsur.c_str());
+		}
+};
