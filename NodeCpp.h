b64daf00bc71720ad4cc14ed67c1027c09b9e1df
东灯 <43312495+cpp-dongdeng@users.noreply.github.com>
2020-02-08 18:15:18 +0800

    Add files via upload

--- NodeCpp.h
+++ NodeCpp.h
@@ -1,17 +1,39 @@
-class Node{
-	public:	
-		string url="";
-		void eval(string js_line){
-			if(url=="")url="eval.js";
-			ofstream out;
-			out.open(url.c_str());
-			out<<js_line;
-			out.close();
-			string rsur="node "+url;
-			system(rsur.c_str());
-		}
-		
-};
+void eval(string js_line,string url){
+	if(url=="")url="eval.js";
+	ofstream out;
+	out.open(url.c_str());
+	out<<js_line;
+	out.close();
+	string rsur="node "+url;
+	system(rsur.c_str());
+	remove(url.c_str());
+}
+int str_to_int(string str){
+	int number=0;
+	for(int i=0;i<str.size();i++)
+		number=(number<<1)+(number<<3)+(str[i]^48);
+	return number;
+}
+string json_in(string json_string,string what,string cfrom_url){
+		ofstream out;
+		string jsurl=cfrom_url+"json.js";
+		out.open(jsurl.c_str());
+		out<<"var fs = require('fs');\n";
+		out<<"var json="<<json_string<<";\n";
+		out<<"fs.writeFile('test.txt',"<<"json."<<what<<",()=>{} );";
+		out.close();
+		string openjs="node "+jsurl;
+		system(openjs.c_str());
+		ifstream in;
+		string end;
+		string txturl=cfrom_url+"test.txt";
+		in.open(txturl.c_str());
+		in>>end;
+		in.close();
+		remove(jsurl.c_str());
+		remove(txturl.c_str());
+		return end;
+}
 class Node_js{
 	public:
 		string jsurl;
@@ -21,8 +43,14 @@ class Node_js{
 			 file<<js_line;
 			 file.close();
 		}
-		void run(){
+		void run(){;
 			string rsur="node "+jsurl;
 			system(rsur.c_str());
 		}
+		void del(){
+			ofstream out;
+			out.open(jsurl.c_str());
+			out<<"";
+			out.close();
+		} 
 };
