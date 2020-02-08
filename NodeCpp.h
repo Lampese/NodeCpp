5ae6f9b5eaa3b6ff48eab9c1de31dca769a0b288
东灯 <43312495+cpp-dongdeng@users.noreply.github.com>
2020-02-08 10:07:42 +0800

    Add files via upload

--- NodeCpp.h
+++ NodeCpp.h
@@ -10,31 +10,8 @@ class Node{
 			string rsur="node "+url;
 			system(rsur.c_str());
 		}
-		void del(){
-			ofstream out;
-			out.open(url.c_str());
-			out<<"";
-			out.close();
-		} 
+		
 };
-string in(string json_string,string what,string cfrom_url){
-		ofstream out;
-		string jsurl=cfrom_url+"json.js";
-		out.open(jsurl.c_str());
-		out<<"var fs = require('fs');\n";
-		out<<"var json="<<json_string<<";\n";
-		out<<"fs.writeFile('test.txt',"<<"json."<<what<<",()=>{} );";
-		out.close();
-		string openjs="node "+jsurl;
-		system(openjs.c_str());
-		ifstream in;
-		string end;
-		string txturl=cfrom_url+"test.txt";
-		in.open(txturl.c_str());
-		in>>end;
-		in.close();
-		return end;
-}
 class Node_js{
 	public:
 		string jsurl;
@@ -48,10 +25,4 @@ class Node_js{
 			string rsur="node "+jsurl;
 			system(rsur.c_str());
 		}
-		void del(){
-			ofstream out;
-			out.open(jsurl.c_str());
-			out<<"";
-			out.close();
-		} 
 };
